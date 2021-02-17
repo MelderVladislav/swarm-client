34cc360d8bfef208c0610dd49fd88204cd02d78f
vladislav.melder@arcadia.spb.ru <vladislav.melder@arcadia.spb.ru>
2021-02-17 21:21:38 +0300

    Start project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'element-plus/lib/theme-chalk/index.css';
+import ElementPlus from 'element-plus'
 
-createApp(App).mount('#app')
+let app = createApp(App)
+
+app.use(ElementPlus);
+
+app.mount('#app')
