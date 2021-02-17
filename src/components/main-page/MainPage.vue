f2cb032a29cbe081bbefd80c46f1f1b5f226da01
vladislav.melder@arcadia.spb.ru <vladislav.melder@arcadia.spb.ru>
2021-02-18 00:22:30 +0300

    Add axios

--- src/components/main-page/MainPage.vue
+++ src/components/main-page/MainPage.vue
@@ -1,6 +1,6 @@
 <template>
   <el-tabs>
-    <el-tab-pane label="Kronartist"></el-tab-pane>
+    <el-tab-pane label="Kronartist"><peers-list/></el-tab-pane>
     <el-tab-pane label="Sanyek">
       <file-uploader/>
     </el-tab-pane>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'element-plus/lib/theme-chalk/index.css';
+import VueAxios from 'vue-axios'
 import ElementPlus from 'element-plus'
+import axios, { AxiosStatic } from 'axios'
 
 let app = createApp(App)
-
 app.use(ElementPlus);
-
+app.use(VueAxios, axios);
 app.mount('#app')
